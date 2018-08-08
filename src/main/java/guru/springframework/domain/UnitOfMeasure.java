6b9de02870860d01ce8cc218725e0f75e743ed9a
Lukasz Rogozinski <lukasz.rogozinski@mobica.com>
2018-08-08 14:18:22 +0200

    added repositories

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -12,6 +12,7 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String uom;
+    private String description;
 
     public Long getId() {
         return id;
@@ -27,5 +28,13 @@ public class UnitOfMeasure {
 
     public void setUom(String uom) {
         this.uom = uom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
